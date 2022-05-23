7211c7801ca55092c03fa79dbfad6d4ed65157b9
alienelena <alienelenaaa@gmail.com>
2022-05-23 19:40:35 +0800

    1 file modified

--- App.tsx
+++ App.tsx
@@ -19,19 +19,35 @@ import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import { purple } from '@mui/material/colors';
 
+const Items = [
+  {
+    name: 'DJI Mini3 Pro',
+    image:
+      'https://www.bhphotovideo.com/images/images1500x1500/dji_cp_ma_00000492_02_mini_3_pro_with_1700260.jpg',
+  },
+  {
+    name: 'DJI Air 2S',
+    image:
+      'https://www.fortress.com.hk/medias/1-inch-CMOS-Sensor-12312360.jpg?context=bWFzdGVyfGZyb250em9vbXw1NDUyNnxpbWFnZS9qcGVnfGZyb250em9vbS9oNDcvaDAyLzk3MjM5MjEyMzU5OTguanBnfDBiODcxZmQ5MWRkYjA5ZDRmZDZmY2U4ZDcyOTQ2ODY4ZWJlMDA5YjEyN2ZlNjRkNjgzMzUxZGRjN2NkNGRmMjc',
+  },
+  {
+    name: 'DJI FPV',
+    image:
+      'https://www.scandinavianphoto.se/globalassets/1050797.jpg?ref=81F07EDA5A&w=960&h=960&mode=max',
+  },
+];
+
 const App = () => {
-  return(
+  return (
     <React.Fragment>
-      <CssBaseline /> 
-        <AppBar position = "relative">
-          <Toolbar>
-            <CameraAltIcon />
-            <Typography variant = "h6">
-              DJI
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraAltIcon />
+          <Typography variant="h6">DJI</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
         <Box
           sx={{
             bgcolor: 'background.paper',
@@ -40,130 +56,114 @@ const App = () => {
           }}
         >
           <div>
-            <Grid container  
-            spacing = {1}
-            direction="column"
-            justifyContent="center"
-            alignItems="center">
+            <Grid
+              container
+              spacing={1}
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
               <Grid item>
-                  <Typography variant = "h4" justify content = "flex-end" color = "textPrimary" gutterButton>
-                    P4 MULTISPECTRAL
-                  </Typography>
+                <Typography
+                  variant="h4"
+                  justify
+                  content="flex-end"
+                  color="textPrimary"
+                  gutterButton
+                >
+                  P4 MULTISPECTRAL
+                </Typography>
               </Grid>
               <Grid item>
-                  <Grid container
-                  direction = "row"
-                  justifyContent = "center"
-                  alignItems ="center">
-                      <Grid item xs = {3}> 
-                      </Grid>
-                      <Grid item xs>
-                        <Typography variant = "h6" align = "center" color = "textPrimary" paragraph> 
-                          Gather precise plant-level data using the P4 Multispectral – a high-precision drone with a seamlessly integrated multispectral imaging system built for agriculture missions, environmental monitoring, and more.
-                        </Typography>
-                      </Grid>
-                      <Grid item xs = {3}>
-                      </Grid>
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Grid item xs={3}></Grid>
+                  <Grid item xs>
+                    <Typography
+                      variant="h6"
+                      align="center"
+                      color="textPrimary"
+                      paragraph
+                    >
+                      Gather precise plant-level data using the P4 Multispectral
+                      – a high-precision drone with a seamlessly integrated
+                      multispectral imaging system built for agriculture
+                      missions, environmental monitoring, and more.
+                    </Typography>
                   </Grid>
+                  <Grid item xs={3}></Grid>
                 </Grid>
-              <Grid container
-                direction = "row"
-                alignItems = "center"
-                justifyContent = "center"
-                spacing = {1}>
-                  <Grid item>
-                    <Button variant = "contained" color = "primary">
-                        More
-                      </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button variant = "outlined" color = "primary">
-                        Contact US
-                      </Button>
-                  </Grid>
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                alignItems="center"
+                justifyContent="center"
+                spacing={1}
+              >
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    More
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Contact US
+                  </Button>
+                </Grid>
               </Grid>
             </Grid>
           </div>
         </Box>
       </main>
-      <Grid 
-      direction="row"
-      container spacing = {6}
-      justifyContent="center"
-      alignItems="center">
-
-        <Grid item>
-            <Card flex = {1} >
+      <Grid
+        direction="row"
+        container
+        spacing={6}
+        justifyContent="center"
+        alignItems="center"
+      >
+        {Items.map(({ name, image }, index) => (
+          <Grid key={index} item>
+            <Card flex={1}>
               <CardMedia
                 component="img"
                 height="250"
-                image="https://www.bhphotovideo.com/images/images1500x1500/dji_cp_ma_00000492_02_mini_3_pro_with_1700260.jpg"
-                alt="DJI Mini3 Pro"
+                image={image}
+                alt={name}
               />
               <CardContent>
-                  <Typography variant="h5" color="textPrimary">
-                    DJI Mini3 Pro
-                  </Typography>
+                <Typography variant="h5" color="textPrimary">
+                  {name}
+                </Typography>
               </CardContent>
               <CardActions>
-                  <Button size="small" variant="contained" color="success">良品</Button>
-                  <Button size="small" variant="contained" color="secondary">二手</Button>
-                  <Button size="small" variant="contained" color="error">不良</Button>
-                  <Button size="small" variant="contained">發霉</Button>
-                  <Button size="small" variant="outlined">缺貨</Button>
+                <Button size="small" variant="contained" color="success">
+                  良品
+                </Button>
+                <Button size="small" variant="contained" color="secondary">
+                  二手
+                </Button>
+                <Button size="small" variant="contained" color="error">
+                  不良
+                </Button>
+                <Button size="small" variant="contained">
+                  發霉
+                </Button>
+                <Button size="small" variant="outlined">
+                  缺貨
+                </Button>
               </CardActions>
             </Card>
-        </Grid>
-
-        <Grid item>
-            <Card >
-              <CardMedia
-                component="img"
-                height="250"
-                image="https://www.fortress.com.hk/medias/1-inch-CMOS-Sensor-12312360.jpg?context=bWFzdGVyfGZyb250em9vbXw1NDUyNnxpbWFnZS9qcGVnfGZyb250em9vbS9oNDcvaDAyLzk3MjM5MjEyMzU5OTguanBnfDBiODcxZmQ5MWRkYjA5ZDRmZDZmY2U4ZDcyOTQ2ODY4ZWJlMDA5YjEyN2ZlNjRkNjgzMzUxZGRjN2NkNGRmMjc"
-                alt="DJI Air 2S"
-              />
-              <CardContent>
-                  <Typography variant="h5" color="textPrimary">
-                    DJI Air 2S
-                  </Typography>
-              </CardContent>
-              <CardActions>
-                  <Button size="small" variant="contained" color="success">良品</Button>
-                  <Button size="small" variant="contained" color="secondary">二手</Button>
-                  <Button size="small" variant="contained" color="error">不良</Button>
-                  <Button size="small" variant="contained">發霉</Button>
-                  <Button size="small" variant="outlined">缺貨</Button>
-              </CardActions>
-            </Card>
-        </Grid>
-
-        <Grid item>
-            <Card >
-              <CardMedia
-                component="img"
-                height="250"
-                image="https://www.scandinavianphoto.se/globalassets/1050797.jpg?ref=81F07EDA5A&w=960&h=960&mode=max"
-                alt="DJI FPV"
-              />
-              <CardContent>
-                  <Typography variant="h5" color="textPrimary">
-                    DJI FPV
-                  </Typography>
-              </CardContent>
-              <CardActions>
-                  <Button size="small" variant="contained" color="success">良品</Button>
-                  <Button size="small" variant="contained" color="secondary">二手</Button>
-                  <Button size="small" variant="contained" color="error">不良</Button>
-                  <Button size="small" variant="contained">發霉</Button>
-                  <Button size="small" variant="outlined">缺貨</Button>
-              </CardActions>
-            </Card>
-        </Grid>
+          </Grid>
+        ))}
       </Grid>
-      
     </React.Fragment>
   );
-}
+};
 
 export default App;
